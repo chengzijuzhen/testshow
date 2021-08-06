a5d3606667bedc51645b2ea363ef651e1b49e7a0
kuangyan <yan.kuang@faw-vw.com>
2021-08-06 10:30:17 +0800

    finish the first demo for router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
+  
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',    
+    component:Login
   }
 ]
 
@@ -23,3 +22,5 @@ const router = createRouter({
 })
 
 export default router
+
+
