0075a60d1792c84221c60495003d1d827621cab3
Ytime <752296035@qq.com>
2021-10-08 23:03:09 +0800

    使用mock数据渲染折线图

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -2,162 +2,195 @@
   <div>
     <h3>图表</h3>
     <div class="container" ref="chartContainer"></div>
-
   </div>
 </template>
 
  <script>
- import * as echarts from 'echarts';
+import * as echarts from "echarts";
+import { res } from "./mockData";
+export default {
+  name: "Chart",
+  data() {
+    return {
+      testData: [],
+    };
+  },
 
-  export default {
-    name: 'Login',
-    data () {
-      return {
-        loginForm: {
-          username: '',
-          password: ''
-        },
-        
+  // dom 元素渲染出来
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(this.$refs.chartContainer);
+
+    this.getLineData().then((d) => {
+      console.log("fetch data", d);
+      const option = this.getOpts(d.data);
+      myChart.setOption(option);
+    });
+    // .then(d => {
+    //   console.log('请求结果数据', d)
+    //   // todo
+    // })
+    // .catch(err => {
+    //   console.log('请求失败', err)
+    //   // todo
+    // })
+  },
+  //定义了登录按钮的点击方法，即向后端 /login 接口发送数据，获得成功的响应后，页面跳转到 /home
+  methods: {
+    // 生成前i天的日期字符串
+    getBeforeDate(i) {
+      var n = i;
+      var s = 0;
+      console.log(n); //
+      var d = new Date();
+      var year = d.getFullYear();
+      var mon = d.getMonth() + 1;
+      var day = d.getDate();
+      if (day <= n) {
+        if (mon > 1) {
+          mon = mon - 1;
+        } else {
+          year = year - 1;
+          mon = 12;
+        }
       }
+      d.setDate(d.getDate() + n); //很重要，+n取得是前一天的时间
+      year = d.getFullYear();
+      mon = d.getMonth() + 1;
+      day = d.getDate();
+      s = (mon < 10 ? "0" + mon : mon) + "-" + (day < 10 ? "0" + day : day);
+      return s;
     },
-      mounted() {
-        // 基于准备好的dom，初始化echarts实例	
 
-	var myChart = echarts.init(document.getElementById('main'));
-	var arr = [];//用来存放最近七天的时间
- 
-	function getBeforeDate(i) {
-		var n = i;
-    var s = 0;
-		console.log(n);//
-		var d = new Date();
-		var year = d.getFullYear();
-		var mon = d.getMonth() + 1;
-		var day = d.getDate();
-		if(day <= n) {
-			if(mon > 1) {
-				mon = mon - 1;
-			} else {
-				year = year - 1;
-				mon = 12;
-			}
-		}
-		d.setDate(d.getDate() + n); //很重要，+n取得是前一天的时间
-		year = d.getFullYear();
-		mon = d.getMonth() + 1;
-		day = d.getDate();
-		s = (mon < 10 ? ('0' + mon) : mon) + "-" + (day < 10 ? ('0' + day) : day);
-		return s;
-	}
- 
-	for(var i = 0; i > -7; i--) {
-		arr.push(getBeforeDate(i));
- 
-	}
-	console.log(arr);
-	var option = {
+    // 获取折线图数据
+    getLineData() {
+      /**
+       * get 请求
+       */
+      // this.$axios
+      //   //post 的异步请求，//这里一般请求后端的接口，接口返回一个json字符串
+      //   .get("/api/getTestData", {
+      //     dt: ["10-01", "10-02"],
+      //   });
 
-          tooltip: {
-              trigger: 'axis',
-              axisPointer: {
-                lineStyle: {
-                  width: 3,
-                  color: '#019688',
-                },
-              },
+      // 模拟请求
+      return Promise.resolve(res);
+    },
+
+    // 根据数据生成折线图配置
+    getOpts(data = []) {
+      //用来存放最近七天的时间
+      var arr = data.map((d) => d.date);
+
+      return {
+        // 鼠标移上去显示的提示信息 popup
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            lineStyle: {
+              width: 3,
+              color: "#019688",
+            },
+          },
         },
+        // 网格样式
         grid: {
-          left: '4%',
-          right: '3%',
-          bottom: '3%',
+          left: "4%",
+          right: "3%",
+          bottom: "3%",
           containLabel: true,
         },
-        color: ['#019688', '#119AC2'],
+        color: ["#019688", "#119AC2"],
+        // x坐标轴配置
         xAxis: [
           {
-            type: 'category',
+            type: "category",
             boundaryGap: false,
-            data: arr.reverse(),
+            data: arr,
             splitLine: {
               show: true,
               lineStyle: {
                 width: 1,
-                type: 'solid',
-                color: 'rgba(226,226,226,0.5)',
+                type: "solid",
+                color: "rgba(226,226,226,0.5)",
               },
             },
-            axisTick: { // 轴刻度线
+            axisTick: {
+              // 轴刻度线
               show: false,
             },
           },
         ],
+        // y坐标轴
         yAxis: [
           {
-            type: 'value',
-            name: '用例个数',
-            max: 0.5,
+            type: "value",
+            name: "百分比",
+            // max: 0.5,
             interval: 0.1,
-            axisTick: { // 轴刻度线
+            axisTick: {
+              // 轴刻度线
               show: false,
             },
             // 刻度文字颜色
-            axisLabel: { color: '#808080' },
+            axisLabel: { color: "#808080" },
             // y轴刻度设置
             axisLine: {
               lineStyle: {
                 width: 2,
-                color: '#019688',
+                color: "#019688",
               },
             },
             // y轴分隔线设置
             splitLine: {
               lineStyle: {
-                color: 'rgba(226,226,226,0.5)',
+                color: "rgba(226,226,226,0.5)",
               },
             },
             // y轴分隔区域设置
             splitArea: {
               show: true,
               areaStyle: {
-                color: ['rgba(250,250,250,0.3)', 'rgba(226,226,226,0.3)'],
+                color: ["rgba(250,250,250,0.3)", "rgba(226,226,226,0.3)"],
               },
             },
           },
           {
-            type: 'value',
-            name: '测量原始值',
+            type: "value",
+            name: "用例个数",
             min: 0,
-            max: 10,
-            interval: 2,
-            axisTick: { // 轴刻度线
+            // max: 10,
+            // interval: 2,
+            axisTick: {
+              // 轴刻度线
               show: false,
             },
-            axisLabel: { color: '#808080' },
+            axisLabel: { color: "#808080" },
             axisLine: {
               lineStyle: {
                 width: 2,
-                color: '#019688',
+                color: "#019688",
               },
             },
             // y轴分隔线设置
             splitLine: {
               lineStyle: {
-                color: 'rgba(226,226,226,0.5)',
+                color: "rgba(226,226,226,0.5)",
               },
             },
             splitArea: {
               show: true,
               areaStyle: {
-                color: ['rgba(250,250,250,0.3)', 'rgba(226,226,226,0.3)'],
+                color: ["rgba(250,250,250,0.3)", "rgba(226,226,226,0.3)"],
               },
             },
           },
         ],
         series: [
           {
-            name: '浓度数据',
-            type: 'line',
-            data: [0.2, 0.049, 0.07, 0.23, 0.25, 0.07, 0.15, 0.162, 0.32, 0.2, 0.06, 0.33],
+            name: "百分比",
+            type: "line",
+            data: data.map((d) => d.percent),
             smooth: true,
             symbolSize: 6,
             areaStyle: {},
@@ -166,31 +199,22 @@
             },
           },
           {
-            name: '日期',
-            type: 'line',
+            name: "用例个数",
+            type: "line",
             yAxisIndex: 1,
-            data: [2.6, 5.9, 9.0, 6.4, 8.7, 0.7, 5.6, 2.2, 0.4, 0.18, 0.24, 0.25],
+            data: data.map((d) => d.testCaseCount),
             smooth: true,
             symbolSize: 6,
             areaStyle: {},
             itemStyle: {
               borderWidth: 2,
             },
-          }],
-
-        };
-        myChart.setOption(option);
-
+          },
+        ],
+      };
+    },
   },
-    //定义了登录按钮的点击方法，即向后端 /login 接口发送数据，获得成功的响应后，页面跳转到 /home
-    methods: {
-
-    } 
-    
-  }
-
-        
-
+};
 </script>
 
 
---
+++ src/views/mockData.js
@@ -0,0 +1,41 @@
+export const res = {
+    msg: 'successful',
+    // 具体数据
+    data: [
+        {
+            "date": "2021-10-02",
+            "testCaseCount": 9,
+            "percent": 0.5981479564606291
+        },
+        {
+            "date": "2021-10-03",
+            "testCaseCount": 14,
+            "percent": 0.19700495181677025
+        },
+        {
+            "date": "2021-10-04",
+            "testCaseCount": 8,
+            "percent": 0.4468569931399349
+        },
+        {
+            "date": "2021-10-05",
+            "testCaseCount": 9,
+            "percent": 0.07179379981857958
+        },
+        {
+            "date": "2021-10-06",
+            "testCaseCount": 6,
+            "percent": 0.09505404068278156
+        },
+        {
+            "date": "2021-10-07",
+            "testCaseCount": 5,
+            "percent": 0.6815967217399168
+        },
+        {
+            "date": "2021-10-08",
+            "testCaseCount": 9,
+            "percent": 0.5916549335766945
+        }
+    ]
+}
